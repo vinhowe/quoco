c7df79e0e135c9f6619f3f8517879f775ad2f0a4
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-04-01 20:20:39 -0600

    Minimal viable version of attention logging system--non-syntax-processing

--- log_app.py
+++ log_app.py
@@ -6,12 +6,18 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.application import get_app
 from prompt_toolkit.key_binding.vi_state import InputMode
 
-from log.secure_term import secure_print, add_lines, clear_term
+from perora.secure_term import secure_print, clear_term
+
+import hashlib
+import time
+
+sha1 = hashlib.sha1()
 
 _log_file_name = "log.txt"
-_max_last_log_entries = 20
+_max_last_log_entries = 40
 
 
+# TODO: Create an API for Vim-like prompts
 def get_prompt_text() -> List[Tuple[str, str]]:
     input_mode_map = {
         InputMode.INSERT: ("bg:ansigreen fg:white bold", "[I]"),
@@ -28,7 +34,7 @@ def get_prompt_text() -> List[Tuple[str, str]]:
     return [
         input_mode,
         ("", " "),
-        ("fg:ansibrightcyan bold", f"log: "),
+        ("fg:ansibrightyellow bold", f"log: "),
     ]
 
 
@@ -40,10 +46,29 @@ def write_to_log(entry):
     now = datetime.now()
     formatted_time_now = now.strftime("%X")
     formatted_date_now = formatted_date(now)
-    line = f'{formatted_date_now} @ {formatted_time_now}: {entry}\n'
+    sum_line = f"{int(time.time())} {entry}"
+    sha1.update(bytes(sum_line, "utf-8"))
+    line = (
+        f"{formatted_date_now} @ {formatted_time_now} .{sha1.hexdigest()}: {entry}\n"
+    )
     with open(_log_file_name, "a") as log_file:
         log_file.write(line)
-    secure_print(f'"{entry}" at {formatted_time_now} on {formatted_date_now}')
+
+
+def format_log_line(line: str) -> str:
+    # [:-1] removes endline char that messes up printing
+    line = line[:-1]
+    line_text = line.split(": ", maxsplit=1)[1]
+    line_time_info = line[11:19]
+    line_id = line[21:61]
+    line_info = f"{line_time_info} ({line_id[:7]})"
+    if line_text.startswith("#"):
+        pound_count = len(line_text.split(" ", maxsplit=1)[0])
+        # It's a comment, skip the timestamp
+        formatted_line = f"{' ' * (len(line_info)-pound_count)} {line_text}"
+    else:
+        formatted_line = f"{line_info}: {line_text}"
+    return formatted_line
 
 
 def print_log_history(n_lines: int):
@@ -60,22 +85,25 @@ def print_log_history(n_lines: int):
         if len(matching_lines) == 0:
             return
         max_last_entries = min(n_lines, len(matching_lines))
-        entries_with_plurality = "entry" if max_last_entries == 1 else "entries"
-        secure_print(
-            f"last {max_last_entries} {entries_with_plurality} ({n_lines} max):"
-        )
-        add_lines()
+        # entries_with_plurality = "entry" if max_last_entries == 1 else "entries"
+        # secure_print(
+        #     f"last {max_last_entries} {entries_with_plurality} ({n_lines} max):"
+        # )
         last_lines = matching_lines[-max_last_entries:]
         for line in last_lines:
-            # Remove extra endline on the end
-            secure_print(line[:-1])
+            secure_print(format_log_line(line))
 
 
 def main() -> None:
     prompt_session = PromptSession()
+    # This seems necessary
     try:
         while True:
+            secure_print()
+            secure_print("perora attention log")
+            secure_print()
             print_log_history(_max_last_log_entries)
+            secure_print()
             entry: str = prompt_session.prompt(
                 get_prompt_text, multiline=False, vi_mode=True
             )
@@ -84,6 +112,7 @@ def main() -> None:
             single_line_entry = entry.replace("\n", "").replace("\r", "").strip()
 
             if single_line_entry.lower() in ["q", "exit", "quit", ":q", "close"]:
+                clear_term()
                 exit()
 
             # Replace all spaces
