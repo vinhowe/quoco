e12b969e4a62aa941c349fc581d5544573eb75e9
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-01-02 20:36:53 -0700

    Remove cruft

--- proto/assignments_manager_snippets.py
+++
@@ -1,4 +0,0 @@
-def minutes(timestr: str): 
-    hours_ct = int(timestr[:timestr.index('h')]) 
-    minutes_ct = int(timestr[timestr.index('h') + 1 : timestr.index('m')]) 
-    return (hours_ct * 60) + minutes_ct              
